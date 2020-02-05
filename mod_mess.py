e4a5f6dd70d59104b5f1e6deb78d5a16229a8e44
Michael Ray <raym3@mail.uc.edu>
2020-02-05 12:44:11 -0500

    Deleted T and Nbar objects. Finished up the main body of code, including filtering process

--- mod_mess.py
+++ mod_mess.py
@@ -1,13 +1,9 @@
 """Module containing messenger method code that has been packaged in a more convenient manner"""
 
-"""Questions: When, in the original messenger.py code, did we ever put in the E mode part of signal covariance?
-
-How do I make the code fully general for it to work with signal covariance matrices that have an inverse, and signal covariance matrices that need a pseudo inverse?"""
-
 import numpy as np
 import healpy as hp
 
-class mmwf:
+class Mmwf:
     """wiener filter by messenger method object. 
 
     Parameters:
@@ -17,49 +13,75 @@ class mmwf:
     cooling - Cooling schedule object
     """
 
-    def __init__(self, Nbar, S, T, data, cooling):
-        self.Nbar = Nbar
-        self.S = S
-        self.T = T
-        self.data = data
-        self.cooling = cooling
+    def __init__(self, N_cov, Sig_cov, Cooling):
+        self.N_cov = Noise_cov
+        self.Sig_cov = Sig_cov
+        self.cooling = Cooling
+        self.T = N_cov.T
+        self.Nbar = N_cov.Nbar
 
     def mat_inverse(self, matrix):
         """Computes the inverse of the given matrix"""
         inv = np.linalg.inverse(matrix)
         return inv
 
-    def do_iteration(self, t, lam, s):
-        tpix = ((self.Nbar.inverse().times(self.data)) + (self.T.lam_inverse(lam).times(s))) * mat_inverse((self.Nbar.inverse() + (self.T.lam_inverse(lam))))
+
+    def do_iteration(self, lam, s, data):
+        """Performs one iteration of the messenger method algorithm.
+
+        Parameters
+        lam - Give a scalar that represents lambda in the messenger method equations
+        s - Give a signal that represents s in the pixel domain. This should be size Npix * 2
+        data - Give a vector that is 3 by Npix in size. This is the I,Q,U map being filtered and data should be in the order of I,Q,U."""
         
+        data_qu = data[self.N.Npix:]
+        def solve_pixeqn(self):
+            t = np.matmul(mat_inverse(self.N_cov.Nbar_inverse() + self.N_cov.lamTpix_inverse(lam)), self.N_cov.Nbarinv_times(data_qu) + self.N_cov.invTpix_times(lam, s))
+            return t
+        
+        def solve_spheqn(self, tsph):
+            sig = np.matmul(mat_inverse(self.Sig_cov.pseudo_inv() + self.N_cov.lamTsph_inverse(lam), self.N_cov.invTsph_times(lam, tsph))
+            return sig
+
+        tpix = solve_pixeqn()
         tpix_q = tpix[:NPIX]
         tpix_u = tpix[NPIX:]
-        t_e_b = hp.map2alm((i_q_u[0], tpix_q, tpix_u), lmax=ELLMAX, pol=True)
+        t_e_b = hp.map2alm((data[0,:], tpix_q, tpix_u), lmax=ELLMAX, pol=True)
         tsph_e = t_e_b[1]
         tsph_b = t_e_b[2]
         tsph = np.concatenate((tsph_e, tsph_b), axis = 0)
         
-        ssph = S.pseudo_inv()
+        ssph = solve_spheqn(tsph)
+        ssph_e = ssph[:self.N_cov.Nsph]
+        ssph_b = ssph[self.N_cov.Nsph:]
+        weiner_iqu = hp.alm2map((t_e_b[0], ssph_e, ssph_b), nside = self.N_cov.Nside, lmax = self.N_cov.ellmax, pol=True)
+        s = np.concatenate((weiner_iqu[1], weiner_iqu[2]), axis = 0)
+                            
+        return s 
 
-    def filter_map(self):
-        for lam in self.cooling:
-            self.do_iteration(self.Nbar, self.S, self.T, self.t, self.data, lam)
+
+    def filter_map_pureB(self, data):
+        s = np.zeros(self.N_cov.Npix * 2)
+        for lam in self.Cooling.lam_list:
+            s = self.do_iteration(lam, s, data)
+        s_final = np.matmul(np.matmul(self.Sig_cov.S, self.Sig_cov.pseudo_inv()), s)
 
 
-class N:
+class Noise_cov:
     """Noise covariance object.
 
     Parameters
     cov_mat - Noise covariance matrix as a numpy array. It will be size NPIX by NPIX, or size NPIX by one if it is diagonal.
     """
 
+
     def __init__(self, Npix):
         self.Npix = Npix
         self.Nside = np.sqrt(Npix/12)
         self.ellmax = 3*self.Nside**2
         ells = np.arange(self.ellmax)                                                                                                                       
-        self.Nsph = np.sum(ells+1)                                                                                                             
-        self.cov_mat = np.ones((Npix*2, Npix*2), dtype = np.float64)
+        self.Nsph = np.sum(ells+1)
+
 
     def make_matrix(self, matrix = None):
         """Sets N.cov_mat to be whatever is given as matrix in the input
@@ -67,89 +89,93 @@ class N:
         Parameters
         matrix - Give the noise covariance matrix you want to use. Size of matrix should be Npix*2 by Npix*2
         """
-        self.cov_mat = matrix
+        self.N = matrix
+        tau = np.min(np.diagonal(self.cov_mat))
+        self.T_pix = np.identity(self.Npix * 2) * tau
+        self.Nbar = self.N - self.T_pix
+        self.T_sph = T_pix * 4 * np.pi / self.Npix
 
-    def inverse(self):
+
+    def N_inverse(self):
         """Returns the inverse of the noise covariance matrix
         """
         inv = np.linalg.inv(self.cov_mat)
         return inv
 
-    def times(self, x):
+
+    def Nbar_inverse(self):
+        """Computes the inverse of the Nbar matrix
+        """
+        inv = np.linalg.inv(self.Nbar)
+        return inv
+
+
+    def T_inverse(self):
+        """Computes the inverse of the T matrix
+        """
+        inv = np.linalg.inv(self.T)
+        return T
+
+
+    def lamTpix_inverse(self, lam):
+        """Computes the inverse of lambda times the T_pix matrix
+        
+        Parameters
+        lam - Give a scalar quantity"""
+        inv = np.linalg.inv(lam * self.T_pix)
+        return inv
+
+
+    def lamTsph_inverse(self, lam):
+        """Computes the inverse of lambda times the T_sph matrix
+        
+        Parameters
+        lam - Give a scalar quantity"""
+        inv = np.linalg.inv(lam * self.T_sph)
+        return inv
+
+
+    def Nbarinv_times(self, x):
         """Returns the matrix product of the noise covariance matrix with the given matrix x"""
-        return np.matmul(self.cov_mat, x)
+        return np.matmul(self.Nbar_inverse(), x)
 
 
-class T:
-    """Messenger field covariance object.
+    def invTpix_times(self, lam, x):
+        """Returns the matrix product of (lam * T_pix)**(-1) with the given matrix x.
+        Give a scalar value for lam."""
+        return np.matmul(lamTpix_inverse(lam), x)
 
-    Parameters
-    N - Noise covariance object
-    """
-    def __init__(self, N):
-        self.Npix = N.Npix
-        self.Nsph = N.Nsph                                                                                                                       
-        self.cov_mat_pix = min(N.cov_mat.diagonal()) * np.identity(N.Npix*2)
-        self.cov_mat_sph = min(N.cov_mat.diagonal()) * np.identity(N.Nsph*2) * (4.0*np.pi) / N.Npix
-
-    def times_lam(self, lam):
-        """Multiplies T matrix by scalar value lambda"""
-        product = lam * self.cov_mat_pix
-        return product
-
-    def lam_inverse(self, lam):
-        """Returns the inverse of T times lambda"""
-        inv = np.linalg.inv(times_lam(lam))
-        return inv
-
-    def times(self, x):
-        """Returns the matrix product of the messenger covariance matrix with the given matrix x"""
-        return np.matmul(self.cov_mat_pix, x)
+    def invTsph_times(self, lam, x):
+        """Returns the matrix product of (lam * T_sph)**(-1) with the given matrix x.
+        Give a scalar for lam"""
+        return np.matmul(lamTsph_inverse(lam), x)
 
 
-
-class Nbar:
-    """Nbar object.
-    
-    Parameters
-    N - Noise covariance object
-    T - Messenger field covariance object
-    """
-
-    def __init__(self, N, T):
-        self.cov_mat = N.cov_mat - T.cov_mat
-
-
-    def inverse(self):
-        inv = np.linalg.inv(self.cov_mat)
-        return inv
-
-    def times(self, x):
-        """Returns the matrix product of the Nbar covariance matrix with the given matrix x"""
-        return np.matmul(self.cov_mat, x)
-
-
-class S:
+class Sig_cov:
     """Signal covariance object.
 
     Parameters
-    cov_mat - Signal covariance matrix as a numpy array. It will be size NPIX by NPIX, or size NPIX by one if it is diagonal.
+    S - Signal covariance matrix as a numpy array. It will be size NPIX by NPIX, or size NPIX by one if it is diagonal.
     """
 
-    def __init__(self, cov_mat):
-        self.cov_mat = cov_mat
+    def __init__(self, S):
+        self.S = S
+
 
     def inverse(self):
-        inv = np.linalg.inv(self.cov_mat)
+        inv = np.linalg.inv(self.S)
         return inv
 
+
     def pseudo_inv(self):
-        """Returns the inverse of the first Nsph entries in the signal covariance and leaves the last Nsph elements alone.
+        """Returns the inverse of the first Nsph entries in the signal covariance and sets the last Nsph elements to zero. This is for a pure B estimator. Thus, it is set up so the first Nsph entries in Sig_cov.S are the E mode components and the last Nsph entries are the B mode components.
         """
-        inv_top = self.cov_mat[:Nsph]**(-1)
-        inv_bottom = self.cov_mat[Nsph:]
+        inv_top = self.S[:Nsph] * 0.0
+        inv_bottom = self.S[Nsph:]**(-1)
         inv = np.concatenate((inv_top, inv_bottom), axis = 0)
         return inv
+
+    
 
 class Cooling:
     """Cooling object. This is how you construct a cooling schedule for lambda in the iterating process.
@@ -161,21 +187,26 @@ class Cooling:
         
 
 
-    def standard_cooling(self, eta):
-        """This function returns a list of lambda values as a numpy array that can be used in the iterative scheme.
+    def standard_cooling(self, eta = 0.7):
+        """This function returns a list of lambda values as a numpy array that can be used in the iterative scheme. Standard cooling begins with lambda at 1300, then decreases to 100 on the second iteration and from then on out multiplies by eta on each iteration. Once the value of lambda drops below 1, 5 lambda = 1 values are added to the list.
 
         Parameters
         eta - Give a numeric value between zero and one. The function starts with lambda at 100 and multiplies lambda by eta on each iteration.
         """
         
         lam_list = []
+        lam_list.append(1300)
         lam = 100
         while lam > 1:
             lam_list.append(lam)
             lam = lam*eta
-        lam_list.append(1)
+        i = 0
+        while i < 5:
+            lam_list.append(1)
+            i += 1
         lam_list = np.array(lam_list)
         return lam_list
+
 
     def linear_lam(self, maxlam, stepsize):
         """This function returns a list of lambda values as a numpy array that can be used in the iterative scheme.
